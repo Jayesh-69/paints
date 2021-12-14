38db832b127e1c4781ce92e66586f89f0aa0699d
Jayesh maheshwari <jayeshmaheshwari555@.com>
2021-12-14 14:26:22 +0530

    added more color

--- utils/buttons.py
+++ utils/buttons.py
@@ -16,12 +16,11 @@ class Button:
         pygame.draw.rect(
             win, self.color, (self.x, self.y, self.width, self.height))
         # to draw the broder
-        # if self.text == None:
-        #     pygame.draw.rect(win, BLACK, (self.x, self.y,
-        #                                   self.width, self.height), 1)
+        pygame.draw.rect(win, BLACK, (self.x, self.y,
+                                      self.width, self.height), 1)
 
         if self.text:
-            button_font = get_font(15)
+            button_font = get_font(18)
             text_surface = button_font.render(self.text, 1, self.text_color)
             win.blit(text_surface, (self.x+self.width/2 - text_surface.get_width() /
                      2, (self.y+self.height/2 - text_surface.get_height()/2)))
